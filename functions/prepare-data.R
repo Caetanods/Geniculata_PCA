523b8f0eb3513ccb037ecd69fbcf63f57ba2043d
Caetanods <caetanods1@gmail.com>
2014-10-06 20:44:31 -0700

    Finish first version of 'sim.geomorph' function. Need testing.

--- functions/prepare-data.R
+++ functions/prepare-data.R
@@ -22,6 +22,7 @@ sim.geomorpho <- function (phy, A, iter = 999){
 	##		'geomorph' by Dean Adams. Please cite the original package and correspondent
 	##		articles. See 'help(compare.evol.rates)' for more info.
 
+	## Check objects block:
     if (length(dim(A)) == 3) {
         if (is.null(dimnames(A)[[3]])) {
             stop("Data matrix does not include taxa names as dimnames for 3rd dimension.")
@@ -50,6 +51,7 @@ sim.geomorpho <- function (phy, A, iter = 999){
         stop("Tree missing some taxa in the data matrix.")
     p <- ncol(x) ## This is the (number of landmarks * 2) + 1
 
+	## Define the sigma.d function:
     sigma.d <- function(phy, x, N) {
         x <- prcomp(x)$x
         ones <- array(1, N)
@@ -68,46 +70,22 @@ sim.geomorpho <- function (phy, A, iter = 999){
     
     sigmad.obs <- sigma.d(phy, x, ntaxa)
     ## return(list(sigma.d = sigmad.obs))  ## The original function ends here and do not do the simulations.
-    }
-    if (nlevels(gp) > 1) {
-        sigmad.obs <- sigma.d(phy, x, ntaxa, gp)
-        rate.mat <- diag(sigmad.obs$sigma.all, p)
-        x.sim <- sim.char(phy, rate.mat, nsim = iter)
-        sig.sim <- 1
-        if (nlevels(gp) > 2) {
-            gp.sig.sim <- array(1, dim = c(dim(sigmad.obs$sigma.gp)[1], 
-                dim(sigmad.obs$sigma.gp)[1]))
-        }
-        rate.val <- rep(0, iter)
-        for (ii in 1:iter) {
-            sigmad.sim <- sigma.d(phy, x.sim[, , ii], ntaxa, 
-                gp)
-            sig.sim <- ifelse(sigmad.sim$ratio >= sigmad.obs$ratio, 
-                sig.sim + 1, sig.sim)
-            rate.val[ii] <- sigmad.sim$ratio
-            if (nlevels(gp) > 2) {
-                gp.sig.sim <- ifelse(sigmad.sim$sigma.gp >= sigmad.obs$sigma.gp, 
-                  gp.sig.sim + 1, gp.sig.sim)
-            }
-        }
+
+    rate.mat <- diag(sigmad.obs$sigma.all, p)
+    x.sim <- sim.char(phy, rate.mat, nsim = iter)
+    sig.sim <- 1
+    rate.val <- rep(0, iter)
+
+	## Do the simulations
+    for (ii in 1:iter) {
+        sigmad.sim <- sigma.d(phy, x.sim[, , ii], ntaxa)
+        sig.sim <- ifelse(sigmad.sim$ratio >= sigmad.obs$ratio, sig.sim + 1, sig.sim)
+        rate.val[ii] <- sigmad.sim$ratio
         sig.sim <- sig.sim/(iter + 1)
-        if (nlevels(gp) > 2) {
-            gp.sig.sim <- gp.sig.sim/(iter + 1)
-            rownames(gp.sig.sim) <- colnames(gp.sig.sim) <- levels(gp)
-            gp.sig.sim[lower.tri(gp.sig.sim)] <- NA
-        }
         rate.val[iter + 1] = sigmad.obs$ratio
         hist(rate.val, 30, freq = TRUE, col = "gray", xlab = "SigmaD ratio")
-        arrows(sigmad.obs$ratio, 50, sigmad.obs$ratio, 5, length = 0.1, 
-            lwd = 2)
-        if (nlevels(gp) > 2) {
-            return(list(sigma.d = sigmad.obs$sigma.all, sigmad.all = sigmad.obs$sigma.d.all, 
-                sigmad.ratio = sigmad.obs$ratio, pvalue = sig.sim, 
-                pairwise.pvalue = gp.sig.sim))
-        }
-        else if (nlevels(gp) == 2) {
-            return(list(sigma.d = sigmad.obs$sigma.all, sigmad.all = sigmad.obs$sigma.d.all, 
+        arrows(sigmad.obs$ratio, 50, sigmad.obs$ratio, 5, length = 0.1, lwd = 2)
+        return(list(sigma.d = sigmad.obs$sigma.all, sigmad.all = sigmad.obs$sigma.d.all, 
                 sigmad.ratio = sigmad.obs$ratio, pvalue = sig.sim))
-        }
     }
 }
