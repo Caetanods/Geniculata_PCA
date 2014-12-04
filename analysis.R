491f4ae7ed43826e1b7dc7f615f2ac049dd785c6
Caetanods <caetanods1@gmail.com>
2014-12-04 11:55:55 -0800

    Change to 20 landmarks on males. Use sliding semilandmarks minimizing binding force.

--- analysis.R
+++ analysis.R
@@ -1,23 +1,29 @@
 ## Prepare morphometric data for analysis.
 ## Landmarks and procrustes analysis exported from MorphoJ.
 
-## Load packages
+## Load packages and functions
 library(geomorph)
 library(geiger)
-
-## Get data and functions
 source("./functions/prepare-data.R")
-ind.male <- read.table("./data/Procrustes_male.txt", header = T, sep = "\t")[,-1]
-ind.female <- read.table("./data/Procrustes_female.txt", header = T, sep = "\t")[,-1]
+
+## Get data
 tr <- read.tree("./data/tnt_geniculata.tre")
+raw.male <- read.table("./data/Raw.coord_male_20lmk.txt", header = T, sep = "\t")[,-1]
+raw.female <- read.table("./data/Raw.coord_female.txt", header = T, sep = "\t")[,-1]
+raw.male <- to.geomorph(raw.male)
+raw.female <- to.geomorph(raw.female)
 
-## Mean value for each coordinate of the procrustes centralized data.
-cord.male <- aggregate(ind.male[,-1], by = list(ind.male$Sps), FUN = mean)
-cord.female <- aggregate(ind.female[,-1], by = list(ind.female$Sps), FUN = mean)
+## Define which landmarks will slide.
+slide <- define.sliders.2d(raw.male[,,1], nsliders = 18)
 
-## Prepare data in correct format.
-m.male <- to.geomorph(cord.male)
-m.female <- to.geomorph(cord.female)
+## Procrustes superposition step with sliding semilandmarks minimizing bending energy.
+ind.male <- gpagen(raw.male, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
+ind.female <- gpagen(raw.female, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
+
+## Species mean value for each coordinate of the procrustes centralized data.
+cord.male <- to.mean.shape(ind.male)
+cord.female <- to.mean.shape(ind.female)
+dimnames(cord.male)[[3]] <- dimnames(cord.female)[[3]]
 
 ## Set phylogenetic labels equal to data:
 tr$tip.label <- gsub("_"," ", tr$tip.label)
@@ -27,27 +33,27 @@ tr.grafen <- compute.brlen(tr)
 
 ## Some plotting:
 
-pdf("Transformed_tree.pdf")
-plot.phylo(tr.grafen, edge.width = 1.5, label.offset = 0.03, font = 4)
-edgelabels(round(tr.grafen$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
-axisPhylo(side = 1)
-title(main = "Branch length computed by Grafen", cex = 0.8)
-dev.off()
+## pdf("Transformed_tree.pdf")
+## plot.phylo(tr.grafen, edge.width = 1.5, label.offset = 0.03, font = 4)
+## edgelabels(round(tr.grafen$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
+## axisPhylo(side = 1)
+## title(main = "Branch length computed by Grafen", cex = 0.8)
+## dev.off()
 
 pdf("PC_phylomorphospace.pdf", width = 16, height = 8)
 par(mfrow = c(1,2))
-plotGMPhyloMorphoSpace(tr, m.male)
+plotGMPhyloMorphoSpace(tr, cord.male)
 title("Males")
-plotGMPhyloMorphoSpace(tr, m.female)
+plotGMPhyloMorphoSpace(tr, cord.female)
 title("Females")
 dev.off()
 
 ## Now using an ultrametric tree:
 pdf("PC_ultrametric_phylomorphospace.pdf", width = 16, height = 8)
 par(mfrow = c(1,2))
-plotGMPhyloMorphoSpace(tr.grafen, m.male)
+plotGMPhyloMorphoSpace(tr.grafen, cord.male)
 title("Males")
-plotGMPhyloMorphoSpace(tr.grafen, m.female)
+plotGMPhyloMorphoSpace(tr.grafen, cord.female)
 title("Females")
 dev.off()
 
@@ -56,35 +62,40 @@ dev.off()
 
 ## Using the morphological integration analysis (Adams and Felice, 2014).
 ## Phylo with branch lengths equal to 1.
-int.unit <- phylo.pls(m.male, m.female, tr, iter=1000, verbose = TRUE)
-## Phylo with ultrametric branch lengths.
-int.grafen <- phylo.pls(m.male, m.female, tr.grafen, iter=1000, verbose = TRUE)
-
 pdf("phylo.pls_unit.pdf")
-phylo.pls(m.male, m.female, tr, iter=1000, verbose = TRUE)
+int.unit <- phylo.pls(cord.male, cord.female, tr, iter=1000, verbose = TRUE)
+dev.off()
+## Phylo with ultrametric branch lengths.
+pdf("phylo.pls_grafen.pdf")
+int.grafen <- phylo.pls(cord.male, cord.female, tr.grafen, iter=1000, verbose = TRUE)
 dev.off()
 
-pdf("phylo.pls_grafen.pdf")
-phylo.pls(m.male, m.female, tr.grafen, iter=1000, verbose = TRUE)
-dev.off()
+## Check pvalue.
+int.unit$pvalue
+int.grafen$pvalue
+## Check PLS correlation.
+int.unit[[1]]
+int.grafen[[1]]
 
 ## Check if data differ from a single rate BM model:
-male.sig.eq <- physignal(tr, m.male, method = "Kmult", iter = 999)
-male.sig.gr <- physignal(tr.grafen, m.male, method = "Kmult", iter = 999)
-female.sig.eq <- physignal(tr, m.female, method = "Kmult", iter = 999)
-female.sig.gr <- physignal(tr.grafen, m.female, method = "Kmult", iter = 999)
+male.sig.eq <- physignal(tr, cord.male, method = "Kmult", iter = 999)
+male.sig.gr <- physignal(tr.grafen, cord.male, method = "Kmult", iter = 999)
+female.sig.eq <- physignal(tr, cord.female, method = "Kmult", iter = 999)
+female.sig.gr <- physignal(tr.grafen, cord.female, method = "Kmult", iter = 999)
 print("Males:")
 matrix(c(male.sig.eq$phy.signal, male.sig.gr$phy.signal, male.sig.eq$pvalue, male.sig.gr$pvalue)
      , byrow = TRUE, ncol = 2, dimnames = list(c("phy.signal","p.value"),c("all.equal","grafen")) )
 print("Females:")
 matrix(c(female.sig.eq$phy.signal, female.sig.gr$phy.signal, female.sig.eq$pvalue, female.sig.gr$pvalue)
      , byrow = TRUE, ncol = 2, dimnames = list(c("phy.signal","p.value"),c("all.equal","grafen")) )
-## Females deviate more from BM than males.
+## We can see that males and females do not show significant phylogenetic signal under BM.
+## Values of K_mult ~0.5 indicates that the shape have more variance than expected under a BM model.
 
 ## Calculating rates of evolution under a BM model (Adams, 2014, Sys Bio).
+
 ## Using the tree with branch lengths equal to 1:
 pdf(file = "Monte_Carlo_unit_plot.pdf")
-res <- geo.comp.rates(tr, m.male, m.female, iter = 1000)
+res <- geo.comp.rates(tr, cord.male, cord.female, iter = 1999)
 dev.off()
 
 ## Results
@@ -93,12 +104,20 @@ res$p.value
 
 ## Using the tree with branch lengths by Grafen:
 pdf(file = "Monte_Carlo_grafen_plot.pdf")
-res <- geo.comp.rates(tr.grafen, m.male, m.female, iter = 1000)
+res <- geo.comp.rates(tr.grafen, cord.male, cord.female, iter = 1999)
 dev.off()
 
 ## Results
 res$sigmaA / res$sigmaB
 res$p.value
+
+## Does the genitalia evolve faster under BM than somatic data?
+## Calculate relative rates of genitalia:
+
+## Get scutelum shape data:
+
+## For males:
+
 
 ## Now I can use the metric of Sidlauskas (2008). Sidlauskas, B. 2008. Continuous and Arrested Morphological Diversification in Sister Clades of Characiform Fishes: A Phylomorphospace Approach. Evolution 62:3135–3156.
 ## The idea is to calculate the density index for males and for females. The calculus that Brian used in this paper was developed for two clades in the same tree, however, in our case we have more than one trait for each tip and a single group including all the tips.
