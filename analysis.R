d11a34df20753c1768a224ea4477c62aa8a4419d
Caetanods <caetanods1@gmail.com>
2014-12-15 14:33:14 -0800

    Add graphs.

--- analysis.R
+++ analysis.R
@@ -7,6 +7,7 @@ library(geiger)
 library(nsprcomp)
 library(parallel)
 library(Matrix)
+library(grDevices)
 source("./functions/prepare-data.R")
 
 ## Get data
@@ -41,15 +42,17 @@ tr.grafen <- compute.brlen(tr)
 
 ## Show the ultrametric tree with branch lengths by Graphen (1989) and the tree
 ##   where all branch lengths are equal to 1.
+pdf("Phylogeny_branch_lengths.pdf", width = 14)
 par(mfrow = c(1,2))
-plot.phylo(tr.grafen, edge.width = 1.5, label.offset = 0.03, font = 4)
-edgelabels(round(tr.grafen$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
-axisPhylo(side = 1)
-title(main = "Branch lengths computed by Grafen", cex = 0.8)
-plot.phylo(tr, edge.width = 1.5, label.offset = 0.03, font = 4)
-edgelabels(round(tr$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
-axisPhylo(side = 1)
-title(main = "Branch lengths all 1", cex = 0.8)
+plot.phylo(tr.grafen, edge.width = 1.5, label.offset = 0.03, font = 4, direction = "upwards")
+#edgelabels(round(tr.grafen$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
+axisPhylo(side = 2)
+#title(main = "Branch lengths computed by Grafen", cex = 0.8)
+plot.phylo(tr, edge.width = 1.5, label.offset = 0.03, font = 4, direction = "upwards")
+#edgelabels(round(tr$edge.length, 2), frame = "none", cex = 0.6, adj = c(0.5,-1))
+axisPhylo(side = 2)
+#title(main = "Branch lengths all 1", cex = 0.8)
+dev.off()
 
 ## Plot the phylomorphospace.
 par(mfrow = c(1,2))
@@ -70,7 +73,6 @@ title("Ultrametric tree: Females")
 ## Check if the male genitalia is the most modular structure.
 ## This is expected if they evolve faster and show more species level variation.
 
-
 ## From geomorph format back to matrix:
 dat.male <- to.matrix(cord.male)
 dat.female <- to.matrix(cord.female)
@@ -85,12 +87,15 @@ female.var.prop <- round(female.nspr$sdev^2 / sum(female.nspr$sdev^2), digits = 
 
 ## Plot the percent of variation explained by each axis for male and female
 ##   genitalia. Note that female genitalia are more autocorrelated.
-plot(1:length(male.var.prop), male.var.prop, type = "b", col = "blue"
-   , ylim = c(0,1.0), xlim = c(0,length(male.var.prop))
-   , xlab = "SPC axis", ylab = "Percent of variation"
-   , main = "Dimensionality of the genitalia")
-lines(1:length(female.var.prop), female.var.prop, type = "b", col = "red")
-legend("topright", legend = c("Male","Female"), text.col = c("blue","red"), bty = "n")
+pdf("SPCA_genitalia.pdf")
+plot(1:length(male.var.prop), male.var.prop, type = "b", pch = 4,
+     , ylim = c(0,1.0), xlim = c(1,length(male.var.prop))
+     , xlab = "SPC axis", ylab = "% variation explained"
+     , axes = FALSE )
+lines(1:length(female.var.prop), female.var.prop, type = "b")
+axis(side = 2); axis(side = 1, at = seq(1, length(male.var.prop)), pos = -0.03)
+legend(x=9, y=0.8, legend = c("Male","Female"), bty = "n", pch = c(4,1))
+dev.off()
 
 ################################################################
 ## Analysis of rate of evolution between genitalia of male and female.
@@ -103,8 +108,60 @@ legend("topright", legend = c("Male","Female"), text.col = c("blue","red"), bty 
 ##   compared are truly multivariate, since Adams (2014) generate a null model in which the
 ##   simulations draw from an uncorrelated multivariatenormal distribution.
 ## Note how the addition of the covariances to the null model changed the distribution:
-comp.graphen <- geo.comp.rates(tr.grafen, cord.male, cord.female, plot = TRUE)
-comp.unit <- geo.comp.rates(tr, cord.male, cord.female, plot = TRUE)
+
+## comp.graphen <- geo.comp.rates(tr.grafen, cord.male, cord.female, plot = FALSE)
+## comp.unit <- geo.comp.rates(tr, cord.male, cord.female, plot = FALSE)
+## save(comp.graphen, comp.unit, file = "./data/male_female_rates.RData")
+load("./data/male_female_rates.RData")
+
+## Make density plot for ultrametric tree:
+pdf("Density_Genitalia_male_female_graphen.pdf")
+plot(density(comp.graphen$null.uncor), zero.line = FALSE, main=""
+   , xlab = expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2))
+   , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+lines(density(comp.graphen$null.cor), lwd = 3)
+obs.rate <- comp.graphen$obs[1] / comp.graphen$obs[2]
+segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
+text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
+legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
+axis(side = 2, at = 0:5); axis(side = 1)
+dev.off()
+
+## For the unit tree:
+pdf("Density_Genitalia_male_female_unit.pdf")
+plot(density(comp.unit$null.uncor), zero.line = FALSE, main=""
+   , xlab = expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2))
+   , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+lines(density(comp.unit$null.cor), lwd = 3)
+obs.rate <- comp.unit$obs[1] / comp.unit$obs[2]
+segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
+text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
+legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
+axis(side = 2, at = 0:5); axis(side = 1)
+dev.off()
+
+## Plate version:
+pdf("Density_Genitalia_male_female_plate.pdf", width = 14)
+par(mfrow = c(1,2))
+plot(density(comp.graphen$null.uncor), zero.line = FALSE, main=""
+   , xlab = expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2))
+   , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+lines(density(comp.graphen$null.cor), lwd = 3)
+obs.rate <- comp.graphen$obs[1] / comp.graphen$obs[2]
+segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
+text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
+legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
+axis(side = 2, at = 0:5); axis(side = 1)
+plot(density(comp.unit$null.uncor), zero.line = FALSE, main=""
+   , xlab = expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2))
+   , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+lines(density(comp.unit$null.cor), lwd = 3)
+obs.rate <- comp.unit$obs[1] / comp.unit$obs[2]
+segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
+text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
+legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
+axis(side = 2, at = 0:5); axis(side = 1)
+dev.off()
 
 ## Using Monte Carlo simulation produces empirical p.values.
 ## Then we analysed multiple times to get a distribution of p.values:
@@ -152,13 +209,20 @@ f.s.unit <- geo.comp.rates(tr.grafen, cord.female, cord.scu.female, plot = TRUE)
 
 ## Using the morphological integration analysis (Adams and Felice, 2014).
 ## Phylo with branch lengths equal to 1.
-pdf("phylo.pls_unit.pdf")
-int.unit <- phylo.pls(cord.male, cord.female, tr, iter=1000, verbose = TRUE)
-dev.off()
+## int.unit <- phylo.pls(cord.male, cord.female, tr, iter=1000, verbose = TRUE)
 ## Phylo with ultrametric branch lengths.
-pdf("phylo.pls_grafen.pdf")
-int.grafen <- phylo.pls(cord.male, cord.female, tr.grafen, iter=1000, verbose = TRUE)
-dev.off()
+## int.graphen <- phylo.pls(cord.male, cord.female, tr.grafen, iter=1000, verbose = TRUE)
+## save(int.graphen, int.unit, file = "./data/male_female_integration.RData")
+load("./data/male_female_integration.RData")
+male.pls.graphen <- int.graphen[[3]]
+female.pls.graphen <- int.graphen[[4]]
+
+plot(female.pls.graphen~male.pls.graphen, axes = FALSE, main = ""
+   , ylab = "Female PLS scores", xlab = "Male PLS scores"
+   , xlim = c(-0.3,0.4), ylim = c(-0.3, 0.4)
+   , pch = 16)
+axis(side = 1); axis(side = 2)
+legend(x = -0.2, y = 0.3, legend = "")
 
 ## Check pvalue.
 int.unit$pvalue
@@ -170,3 +234,12 @@ int.grafen[[1]]
 ## Now I can use the metric of Sidlauskas (2008). Sidlauskas, B. 2008. Continuous and Arrested Morphological Diversification in Sister Clades of Characiform Fishes: A Phylomorphospace Approach. Evolution 62:3135–3156.
 ## The idea is to calculate the density index for males and for females. The calculus that Brian used in this paper was developed for two clades in the same tree, however, in our case we have more than one trait for each tip and a single group including all the tips.
 ## We might need to modify his calculations to do something more direct in this case.
+
+
+
+
+
+
+
+
+
