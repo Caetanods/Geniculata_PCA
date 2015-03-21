19f0a013240535fc9c456e294a935b0c2a8d4aa9
Caetanods <caetanods1@gmail.com>
2015-03-20 17:02:02 -0700

    Simulation for the performance of the 'geo.comp.rates' function.

--- dimentionality_simulation.R
+++ dimentionality_simulation.R
@@ -1,7 +1,10 @@
-## This is to simulate the behaviour of sigma mult and the sigma mult null model simulated with a diagonal matrix (i.e., no covariation) given two set of traits with different dimentionality.
+## This is to simulate the behaviour of sigma mult and the sigma mult null model simulated
+##         with a diagonal matrix (i.e., no covariation) given two set of traits with
+##         different dimentionality.
 
-## Start with a shape. Evolve the shape. Create a new dataset only by changing the dimentionality of the trait. Then estimate sigma mult, simulate the null, check if it works.
-## Add the information of the R matrix in the simulation step. How to get the R matrix from the Adams method? Maybe from the covariance matrix of the correlated evolution paper.
+## We start with a shape. Evolve the shape. Create a new dataset only by changing
+##         the dimentionality of the trait. Then estimate sigma mult, simulate the null
+##         and check whether the method gives the expected result.
 
 library(geomorph)
 library(geiger)
@@ -9,66 +12,65 @@ library(MASS)
 library(phytools)
 
 source("./functions/prepare-data.R")
+source("./functions/analysis.R")
 source("./functions/simulations.R")
 
 ## Simulate the tree:
-phy <- sim.bdtree(stop = "taxa", n = 100)
+phy <- sim.bdtree(stop = "taxa", n = 50)
 phy <- rescale(phy, model = "depth", 1)
 plot(phy, direction = "upward"); axisPhylo(side = 2)
 
 #############################################################################
 
-## Generate R matrices with the same parameters:
-## No difference in dimensionality:
-R1 <- Posdef(10, rexp(10, 5/100))
-R2 <- Posdef(10, rexp(10, 1/100))
+## Generate R matrices. Different rates and dimensionality.
 
-## Use R matrices to simulate data, estimate sigma_mult and simulate sigma_mult
-##     distributions under BM.
-tr1 <- sim.char(phy, R1, model = "BM")[,,1]
-tr1 <- to.geomorph(cbind(rownames(tr1),tr1))
-tr2 <- sim.char(phy, R2, model = "BM")[,,1]
-tr2 <- to.geomorph(cbind(rownames(tr2),tr2))
-comp <- geo.comp.corr.rates(phy, tr1, tr2)
+R1 <- Posdef(10, rexp(10, 2))
 
-cc1 <- comp.adams.harmon(phy, tr1, 100)
-cc2 <- comp.adams.harmon(phy, tr2, 100)
+#####################################################
+## Here we make simulations using diagonal matrices. This R matrices do not have a realistic
+##     covariance structure, however, it is difficult to evaluate the expected difference
+##     in rates of evolution under BM for more realistic covariance matrices since
+##     the structure of covariance may result in differences of rates between matrices
+##     that were simulated to be the same.
+## The results of those simulations should hold if one include realistic R matrices that
+##     are known to have a given evolutionary rate relationship.
 
-## We expect no difference between rates under both null models (correlated or not).
-dif.uncor <- cc1$sigma.uncor.sim / cc2$sigma.uncor.sim
-dif.cor <- cc1$sigma.cor.sim / cc2$sigma.cor.sim
-pdf("Sims_cor_uncor_no_difference.pdf", width = 14)
-par(mfrow = c(1,2))
-hist(dif.uncor, breaks = 50, border = "blue", main = "Uncorrelated", xlab = "sigma_mult")
-abline(v=cc1$sigma.obs / cc2$sigma.obs)
-hist(dif.cor, breaks = 50, border = "blue", main = "Correlated", xlab = "sigma_mult")
-abline(v=cc1$sigma.obs / cc2$sigma.obs)
-dev.off()
+## Doing simulations with diagonal matrices:
+Rdiag1 <- diag(0.05, 10, 10)
+Rdiag2 <- diag(0.10, 10, 10)
+Rdiag3 <- diag(0.05, 20, 20)
+Rdiag4 <- diag(0.10, 20, 20)
 
-##########################################
-## This part here came from another script.
-## Simulating under known R matrices and phylogenies:
+## Simulate the traits under a BM model:
+trd1 <- sim.char(phy, Rdiag1, model = "BM")[,,1]
+trd2 <- sim.char(phy, Rdiag1, model = "BM")[,,1]
+trd3 <- sim.char(phy, Rdiag2, model = "BM")[,,1]
+trd4 <- sim.char(phy, Rdiag3, model = "BM")[,,1]
+trd5 <- sim.char(phy, Rdiag4, model = "BM")[,,1]
 
-phy <- sim.bdtree(stop="taxa")
-phy <- rescale(phy, "depth", 1)
+## Prepare the data to run the 'geo.comp.rates' function:
+trd1 <- to.geomorph(cbind(rownames(trd1), trd1))
+trd2 <- to.geomorph(cbind(rownames(trd2), trd2))
+trd3 <- to.geomorph(cbind(rownames(trd3), trd3))
+trd4 <- to.geomorph(cbind(rownames(trd4), trd4))
+trd5 <- to.geomorph(cbind(rownames(trd5), trd5))
 
-## Generate R matrices with the same parameters:
-R1 <- Posdef(10)
-R2 <- Posdef(10)
-## Use R matrices to simulate data, estimate sigma_mult and simulate sigma_mult
-##     distributions under BM.
-tr1 <- sim.to.geo(phy, R1)
-cc1 <- comp.adams.harmon(phy, tr1, 1000)
-tr2 <- sim.to.geo(phy, R2)
-cc2 <- comp.adams.harmon(phy, tr2, 1000)
+## Same rate and same dimensions.
+diag.comp1 <- geo.comp.rates(phy, trd1, trd2, plot = TRUE)
+diag.comp1$obs
 
-## We expect no difference between rates under both null models (correlated or not).
-dif.uncor <- cc1$sigma.uncor.sim / cc2$sigma.uncor.sim
-dif.cor <- cc1$sigma.cor.sim / cc2$sigma.cor.sim
-pdf("Sims_cor_uncor_no_difference.pdf", width = 14)
-par(mfrow = c(1,2))
-hist(dif.uncor, breaks = 50, border = "blue", main = "Uncorrelated", xlab = "sigma_mult")
-abline(v=cc1$sigma.obs / cc2$sigma.obs)
-hist(dif.cor, breaks = 50, border = "blue", main = "Correlated", xlab = "sigma_mult")
-abline(v=cc1$sigma.obs / cc2$sigma.obs)
-dev.off()
+## Same rate and different dimensions. (A is smaller)
+diag.comp2 <- geo.comp.rates(phy, trd1, trd4, plot = TRUE)
+diag.comp2$obs
+
+## Different rates and same dimensions. (A is slower.)
+diag.comp3 <- geo.comp.rates(phy, trd1, trd3, plot = TRUE)
+diag.comp3$obs
+
+## Different rates and different dimensions. (A is faster and smaller).
+diag.comp4 <- geo.comp.rates(phy, trd3, trd4, plot = TRUE)
+diag.comp4$obs
+
+## Different rates and different dimensions. (A is faster and larger).
+diag.comp5 <- geo.comp.rates(phy, trd5, trd1, plot = TRUE)
+diag.comp5$obs
