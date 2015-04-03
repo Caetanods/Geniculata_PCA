8e2121340755cb34da37755e52440c93410fa973
Caetanods <caetanods1@gmail.com>
2015-04-03 15:04:27 -0700

    Now load data already processed

--- Analysis_compare_rates.R
+++ Analysis_compare_rates.R
@@ -9,34 +9,10 @@ library(parallel)
 library(Matrix)
 library(grDevices)
 source("./functions/analysis.R")
+source("./functions/prepare-data.R")
 
-
-## Get data
-tr <- read.tree("./data/tnt_geniculata.tre")
-raw.male <- read.table("./data/Raw.coord_male_20lmk.txt", header = T, sep = "\t")[,-1]
-raw.female <- read.table("./data/Raw.coord_female.txt", header = T, sep = "\t")[,-1]
-raw.male <- to.geomorph(raw.male)
-raw.female <- to.geomorph(raw.female)
-
-##############################################################
-## Define which landmarks will slide.
-##slide <- define.sliders.2d(raw.male[,,1], nsliders = 18)
-slide <- as.matrix(read.csv("./data/curveslide.csv"))
-
-## Procrustes superposition step with sliding semilandmarks minimizing bending energy.
-ind.male <- gpagen(raw.male, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-ind.female <- gpagen(raw.female, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-
-## Species mean value for each coordinate of the procrustes centralized data.
-cord.male <- to.mean.shape(ind.male)
-cord.female <- to.mean.shape(ind.female)
-dimnames(cord.male)[[3]] <- dimnames(cord.female)[[3]]
-
-## Set phylogenetic labels equal to data:
-tr$tip.label <- gsub("_"," ", tr$tip.label)
-
-## Get ultrametric trees by branch trasformations:
-tr.grafen <- compute.brlen(tr)
+## Get data. See 'Prepare_data.R':
+load("./data/Geniculata_data.RData")
 
 #################################################
 ## Some plotting:
@@ -53,15 +29,15 @@ tr.grafen <- compute.brlen(tr)
 
 ## Plot the phylomorphospace.
 ## par(mfrow = c(1,2))
-## plotGMPhyloMorphoSpace(tr, cord.male)
+## plotGMPhyloMorphoSpace(tr, cord.ind.male)
 ## title("Branch lengths equal 1: Males")
-## plotGMPhyloMorphoSpace(tr, cord.female)
+## plotGMPhyloMorphoSpace(tr, cord.ind.female)
 ## title("Branch lengths equal 1: Females")
 
 ## par(mfrow = c(1,2))
-## plotGMPhyloMorphoSpace(tr.grafen, cord.male)
+## plotGMPhyloMorphoSpace(tr.grafen, cord.ind.male)
 ## title("Ultrametric tree: Males")
-## plotGMPhyloMorphoSpace(tr.grafen, cord.female)
+## plotGMPhyloMorphoSpace(tr.grafen, cord.ind.female)
 ## title("Ultrametric tree: Females")
 
 ###################################################
@@ -71,8 +47,8 @@ tr.grafen <- compute.brlen(tr)
 ## This is expected if they evolve faster and show more species level variation.
 
 ## From geomorph format back to matrix:
-dat.male <- to.matrix(cord.male)
-dat.female <- to.matrix(cord.female)
+dat.male <- to.matrix(cord.ind.male)
+dat.female <- to.matrix(cord.ind.female)
 
 ## Calculate Sparse Principal Component Analysis (SPCA):
 ## This analysis is NOT analogous to a phylogenetic PCA.
@@ -109,115 +85,54 @@ female.var.prop <- round(female.nspr$sdev^2 / sum(female.nspr$sdev^2), digits = 
 ##   simulations draw from an uncorrelated multivariate-normal distribution.
 ## Note how the addition of the covariances to the null model changed the distribution:
 
-## comp.graphen <- geo.comp.rates(tr.grafen, cord.male, cord.female, plot = FALSE)
-## comp.unit <- geo.comp.rates(tr, cord.male, cord.female, plot = FALSE)
+## comp.graphen <- geo.comp.rates(tr.grafen, cord.ind.male, cord.ind.female, plot = FALSE)
+## comp.unit <- geo.comp.rates(tr, cord.ind.male, cord.ind.female, plot = FALSE)
 ## save(comp.graphen, comp.unit, file = "./data/male_female_rates.RData")
 load("./data/male_female_rates.RData")
 
+## plot(dd.null.uncor, zero.line = FALSE, main="", xlab = "", ylab = ""
+##    , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+
 ## Make density plot for ultrametric tree:
-## pdf("manuscript_figures/Density_Genitalia_male_female_graphen.pdf")
-## par(mar = c(4.0,4.0,2.0,2.0))
-## plot(density(comp.graphen$null.uncor), zero.line = FALSE, main=""
-##    , xlab = "", ylab = ""
-##    , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
-## lines(density(comp.graphen$null.cor), lwd = 3)
-## obs.rate <- comp.graphen$obs[1] / comp.graphen$obs[2]
-## segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
-## text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
-## legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
-## axis(side = 2, at = 0:5); axis(side = 1)
-## mtext(expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2)), side = 1, line = 2.5)
-## mtext("Density", side = 2, line = 2)
-## dev.off()
-
-## ## For the unit tree:
-## pdf("manuscript_figures/Density_Genitalia_male_female_unit.pdf")
-## par(mar = c(4.0,4.0,2.0,2.0))
-## plot(density(comp.unit$null.uncor), zero.line = FALSE, main=""
-##    , xlab = "", ylab = ""
-##    , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
-## lines(density(comp.unit$null.cor), lwd = 3)
-## obs.rate <- comp.unit$obs[1] / comp.unit$obs[2]
-## segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
-## text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
-## legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
-## axis(side = 2, at = 0:5); axis(side = 1)
-## mtext(expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2)), side = 1, line = 2.5)
-## mtext("Density", side = 2, line = 2)
-## dev.off()
-
-## ## Plate version:
-## pdf("manuscript_figures/Density_Genitalia_male_female_plate.pdf", width = 14)
-## par(mfrow = c(1,2))
-## par(mar = c(4.0,4.0,2.0,0.0))
-## plot(density(comp.graphen$null.uncor), zero.line = FALSE, main=""
-##    , xlab = "", ylab = ""
-##    , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
-## lines(density(comp.graphen$null.cor), lwd = 3)
-## obs.rate <- comp.graphen$obs[1] / comp.graphen$obs[2]
-## segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
-## text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
-## legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
-## axis(side = 2, at = 0:5); axis(side = 1)
-## mtext(expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2)), side = 1, line = 2.5)
-## mtext("Density", side = 2, line = 2)
-## par(mar = c(4.0,3.0,2.0,1.0))
-## plot(density(comp.unit$null.uncor), zero.line = FALSE, main=""
-##    , xlab = "", ylab = ""
-##    , xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
-## lines(density(comp.unit$null.cor), lwd = 3)
-## obs.rate <- comp.unit$obs[1] / comp.unit$obs[2]
-## segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
-## text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
-## legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
-## axis(side = 2, at = 0:5); axis(side = 1)
-## mtext(expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2)), side = 1, line = 2.5)
-## mtext("Density", side = 2, line = 2)
-## dev.off()
-
-## Using Monte Carlo simulation produces empirical p.values.
-## Then we analysed multiple times to get a distribution of p.values:
-## NOTE: We do not need to do this. We need to increase the number of simulations in the distribution
-##       of the Monte Carlo for each analysis.
-## p.graphen <- mclapply(1:20, FUN = function(x) geo.comp.rates(tr.grafen, cord.male, cord.female), mc.cores = 2)
-## p.graphen <- lapply(p.graphen, FUN = function(x) x$p.value)
-## p.graphen <- do.call(rbind, p.graphen)
-## p.unit <- mclapply(1:20, FUN = function(x) geo.comp.rates(tr, cord.male, cord.female), mc.cores = 2)
-## p.unit <- lapply(p.unit, FUN = function(x) x$p.value)
-## p.unit <- do.call(rbind, p.unit)
-## ## Summary of distributions of p.value using the ultrametric and the unit tree.
-## summary(p.graphen)
-## summary(p.unit)
+pdf("manuscript_figures/Density_Genitalia_male_female_graphen.pdf")
+par(mar = c(4.0,4.0,2.0,2.0))
+dd.null.uncor <- density(comp.graphen$null.uncor, from = 0, to = 4)
+dd.null.cor <- density(comp.graphen$null.cor, from = 0, to = 4)
+qq.uncor <- quantile(comp.graphen$null.uncor, probs = 0.95)
+qq.cor <- quantile(comp.graphen$null.cor, probs = 0.95)
+ii.uncor <- min(which(dd.null.uncor$x >= qq.uncor))
+ii.cor <- min(which(dd.null.cor$x >= qq.cor))
+plot(NA, main="", xlab = "", ylab = "", xlim = c(0,4), ylim = c(0,5), axes=FALSE, lty = 3, lwd = 3)
+polygon(x = dd.null.uncor$x[c(ii.uncor, ii.uncor:length(dd.null.uncor$x), length(dd.null.uncor$x) )]
+      , y = c(0, dd.null.uncor$y[ii.uncor:length(dd.null.uncor$x)], 0 )
+      , col="grey", border = "white")
+polygon(x = dd.null.cor$x[c(ii.cor, ii.cor:length(dd.null.cor$x), length(dd.null.cor$x) )]
+      , y = c(0, dd.null.cor$y[ii.cor:length(dd.null.cor$x)], 0 )
+      , col="grey", border = "white")
+lines(dd.null.uncor, lty = 3, lwd = 3)
+lines(dd.null.cor, lwd = 3)
+obs.rate <- comp.graphen$obs[1] / comp.graphen$obs[2]
+segments(x0 = obs.rate, y0 = 0, x1 = obs.rate, y1 = 1.5, lwd = 2, lty = 1)
+text(x = obs.rate, y = 1.7, labels = round(obs.rate, 1))
+legend(x = 2.5, y = 4, legend = c("Uncorrelated","Correlated"), bty = "n", lty = c(3,1), lwd = 3)
+axis(side = 2, at = 0:5); axis(side = 1)
+mtext(expression(paste(sigma["mult.M"]^2 / sigma["mult.F"]^2)), side = 1, line = 2.5)
+mtext("Density", side = 2, line = 2)
+dev.off()
 
 ################################################################
 ## Does the genitalia evolve faster under BM than somatic data?
 
-## Analysis of rate of evolution between genitalia and scutelum.
-
-## Get scutelum shape data:
-raw.scu <- read.table("./data/Raw_scutelum_male_female.txt", header=T, sep="\t", stringsAsFactors=FALSE)[,-1]
-raw.scu.male <- raw.scu[which(raw.scu$sexo == "m"),-2]
-raw.scu.female <- raw.scu[which(raw.scu$sexo == "f"),-2]
-raw.scu.male <- to.geomorph(raw.scu.male)
-raw.scu.female <- to.geomorph(raw.scu.female)
-
-## Procrustes superposition step with sliding semilandmarks minimizing bending energy.
-## Again, we can use the same slide matrix from the previous datasets.
-ind.scu.male <- gpagen(raw.scu.male, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-ind.scu.female <- gpagen(raw.scu.female, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-
-## Species mean value for each coordinate of the procrustes centralized data.
-cord.scu.male <- to.mean.shape(ind.scu.male)
-cord.scu.female <- to.mean.shape(ind.scu.female)
-
+## Analysis of rate of evolution between genitalia and somatic traits.
 ## Relative rates in function of the scutellum for males and females:
 ## Note that the rates of the genitalia are much faster.
 ## For the ultrametric tree:
-## m.s.graphen <- geo.comp.rates(tr.grafen, cord.male, cord.scu.male, plot = TRUE)
-## f.s.graphen <- geo.comp.rates(tr.grafen, cord.female, cord.scu.female, plot = TRUE)
+
+## m.s.graphen <- geo.comp.rates(tr.grafen, cord.ind.male, cord.scu.male, plot = TRUE)
+## f.s.graphen <- geo.comp.rates(tr.grafen, cord.ind.female, cord.scu.female, plot = TRUE)
 ## ## For the tree with branch lengths all equal to 1:
-## m.s.unit <- geo.comp.rates(tr.grafen, cord.male, cord.scu.male, plot = TRUE)
-## f.s.unit <- geo.comp.rates(tr.grafen, cord.female, cord.scu.female, plot = TRUE)
+## m.s.unit <- geo.comp.rates(tr, cord.ind.male, cord.scu.male, plot = TRUE)
+## f.s.unit <- geo.comp.rates(tr, cord.ind.female, cord.scu.female, plot = TRUE)
 ## save(m.s.graphen, f.s.graphen, m.s.unit, f.s.unit, file = "./data/genitalia_scutelum_rates.RData")
 load("./data/genitalia_scutelum_rates.RData")
 
@@ -228,28 +143,12 @@ f.s.graphen$p.value ## Female in ultrametric tree.
 m.s.unit$p.value ## Male in speciational tree.
 f.s.unit$p.value ## Female in speciational tree.
 
-## Analysis of rate of evolution between genitalia and the metacoxa.
-## Note that the metacoxa has linear measurements.
-meta <- read.csv("./data/metacoxa_linear.csv")
-agg <- aggregate(meta$right.metacoxae, by = list(meta$sex, meta$species), FUN = mean)
-mm <- which(agg$Group.1 == "m")
-ff <- which(agg$Group.1 == "f")
-m.meta <- agg$x[mm] ## Mean spp values for males.
-names(m.meta) <- agg$Group.2[mm]
-f.meta <- agg$x[ff] ## Mean spp values for females.
-names(f.meta) <- agg$Group.2[ff]
+## Comparing rates of evolution between the pronotum and the genitalia:
 
-## IMPORTANT: The analysis below are biased. The 'geo.vec.comp.rates' function will return
-##            a (near) significant difference between the rates even if the trait were simulated
-##            under the null model. Maybe the is a result of testing datasets with very different
-##            dimentionality.
-
-## Testing rates differences with raw values:
-m.f.graphen <- geo.vec.comp.rates(tr.grafen, m.meta, cord.male, plot = TRUE)
-f.f.graphen <- geo.vec.comp.rates(tr.grafen, f.meta, cord.female, plot = TRUE)
-m.f.unit <- geo.vec.comp.rates(tr, m.meta, cord.male, plot = TRUE)
-f.f.unit <- geo.vec.comp.rates(tr, f.meta, cord.female, plot = TRUE)
-
-## Testing rates differences with centred values:
-scale.m.f.graphen <- geo.vec.comp.rates(tr.grafen, scale(m.meta)[,1], cord.male, plot = TRUE)
-log.m.f.graphen <- geo.vec.comp.rates(tr.grafen, log(m.meta), cord.male, plot = TRUE)
+## For the ultrametric tree:
+m.p.graphen <- geo.comp.rates(tr.grafen, cord.ind.male, cord.pro.male, plot = TRUE)
+f.p.graphen <- geo.comp.rates(tr.grafen, cord.ind.female, cord.pro.female, plot = TRUE)
+## For the tree with branch lengths all equal to 1:
+m.p.unit <- geo.comp.rates(tr, cord.ind.male, cord.pro.male, plot = TRUE)
+f.p.unit <- geo.comp.rates(tr, cord.ind.female, cord.pro.female, plot = TRUE)
+save(m.p.graphen, f.p.graphen, m.p.unit, f.p.unit, file = "./data/genitalia_pronotum_rates.RData")
--- Analysis_pls_integration.R
+++ Analysis_pls_integration.R
@@ -11,75 +11,38 @@ library(grDevices)
 source("./functions/analysis.R")
 source("./functions/prepare-data.R")
 
-## Get phylogeny
-tr <- read.tree("./data/tnt_geniculata.tre")
-
-## Get genitalia data
-raw.male <- read.table("./data/Raw.coord_male_20lmk.txt", header = T, sep = "\t")[,-1]
-raw.female <- read.table("./data/Raw.coord_female.txt", header = T, sep = "\t")[,-1]
-raw.male <- to.geomorph(raw.male)
-raw.female <- to.geomorph(raw.female)
-
-## Get scutelum data
-raw.scu <- read.table("./data/Raw_scutelum_male_female.txt", header=T, sep="\t", stringsAsFactors=FALSE)[,-1]
-raw.scu.male <- raw.scu[which(raw.scu$sexo == "m"),-2]
-raw.scu.female <- raw.scu[which(raw.scu$sexo == "f"),-2]
-raw.scu.male <- to.geomorph(raw.scu.male)
-raw.scu.female <- to.geomorph(raw.scu.female)
-
-## Decrease data for 20 landmarks
-scu.male <- array(dim = c(20, 2, 63) , dimnames = dimnames(raw.scu.male))
-scu.female <- array(dim = c(20, 2, 97), dimnames = dimnames(raw.scu.female) )
-
-for(i in 1:dim(raw.scu.male)[3]){
-    scu.male[,,i] <- raw.scu.male[-seq(2, 40, by = 2),,i]
-}
-for(i in 1:dim(raw.scu.female)[3]){
-    scu.female[,,i] <- raw.scu.female[-seq(2, 40, by = 2),,i]
-}
-
-
-##############################################################
-## Define which landmarks will slide.
-## slide <- define.sliders.2d(raw.male[,,1], nsliders = 18)
-slide <- as.matrix(read.csv("./data/curveslide_20.csv"))
-
-## Procrustes superposition step with sliding semilandmarks minimizing bending energy.
-ind.male <- gpagen(raw.male, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-ind.female <- gpagen(raw.female, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-scu.male <- gpagen(scu.male, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-scu.female <- gpagen(scu.female, ProcD = FALSE, ShowPlot = FALSE, curves = slide)
-
-## Species mean value for each coordinate of the procrustes centralized data.
-cord.ind.male <- to.mean.shape(ind.male)
-cord.ind.female <- to.mean.shape(ind.female)
-cord.scu.male <- to.mean.shape(scu.male)
-cord.scu.female <- to.mean.shape(scu.female)
-dimnames(cord.ind.male)[[3]] <- dimnames(cord.ind.female)[[3]]
-
-## Set phylogenetic labels equal to data:
-tr$tip.label <- gsub("_"," ", tr$tip.label)
-
-## Get ultrametric trees by branch trasformations:
-tr.grafen <- compute.brlen(tr)
+## Get data. See 'Prepare_data.R':
+load("./data/Geniculata_data.RData")
 
 ## Get phylogenetic contrasts:
 mm.ind <- to.matrix(cord.ind.male)
 mf.ind <- to.matrix(cord.ind.female)
 mm.scu <- to.matrix(cord.scu.male)
 mf.scu <- to.matrix(cord.scu.female)
+mm.pro <- to.matrix(cord.pro.male)
+mf.pro <- to.matrix(cord.pro.female)
+mm.jug <- to.matrix(cord.jug.male)
+mf.jug <- to.matrix(cord.jug.female)
 
 ## Use untrametric tree:
 pic.grafen.ind.m <- sapply(1:dim(mm.ind)[2], function(x) pic(mm.ind[,x], tr.grafen) )
 pic.grafen.ind.f <- sapply(1:dim(mf.ind)[2], function(x) pic(mf.ind[,x], tr.grafen) )
 pic.grafen.scu.m <- sapply(1:dim(mm.scu)[2], function(x) pic(mm.scu[,x], tr.grafen) )
 pic.grafen.scu.f <- sapply(1:dim(mf.scu)[2], function(x) pic(mf.scu[,x], tr.grafen) )
+pic.grafen.pro.m <- sapply(1:dim(mm.pro)[2], function(x) pic(mm.pro[,x], tr.grafen) )
+pic.grafen.pro.f <- sapply(1:dim(mf.pro)[2], function(x) pic(mf.pro[,x], tr.grafen) )
+pic.grafen.jug.m <- sapply(1:dim(mm.jug)[2], function(x) pic(mm.jug[,x], tr.grafen) )
+pic.grafen.jug.f <- sapply(1:dim(mf.jug)[2], function(x) pic(mf.jug[,x], tr.grafen) )
 
 ## Use unit tree:
 pic.unit.ind.m <- sapply(1:dim(mm.ind)[2], function(x) pic(mm.ind[,x], tr) )
 pic.unit.ind.f <- sapply(1:dim(mf.ind)[2], function(x) pic(mf.ind[,x], tr) )
 pic.unit.scu.m <- sapply(1:dim(mm.scu)[2], function(x) pic(mm.scu[,x], tr) )
 pic.unit.scu.f <- sapply(1:dim(mf.scu)[2], function(x) pic(mf.scu[,x], tr) )
+pic.unit.pro.m <- sapply(1:dim(mm.pro)[2], function(x) pic(mm.pro[,x], tr) )
+pic.unit.pro.f <- sapply(1:dim(mf.pro)[2], function(x) pic(mf.pro[,x], tr) )
+pic.unit.jug.m <- sapply(1:dim(mm.jug)[2], function(x) pic(mm.jug[,x], tr) )
+pic.unit.jug.f <- sapply(1:dim(mf.jug)[2], function(x) pic(mf.jug[,x], tr) )
 
 ## Two Block PLS analysis for ultrametric and speciational trees:
 
@@ -87,20 +50,63 @@ pic.unit.scu.f <- sapply(1:dim(mf.scu)[2], function(x) pic(mf.scu[,x], tr) )
 pls.ind.grafen <- two.b.pls(pic.grafen.ind.m, pic.grafen.ind.f, verbose = TRUE)
 pls.ind.unit <- two.b.pls(pic.unit.ind.m, pic.unit.ind.f, verbose = TRUE)
 
-## Male genitalia vs. male scutellum:
+## Male genitalia vs. male scutellum and pronotum:
 pls.scu.m.grafen <- two.b.pls(pic.grafen.ind.m, pic.grafen.scu.m, verbose = TRUE)
 pls.scu.m.unit <- two.b.pls(pic.unit.ind.m, pic.unit.scu.m, verbose = TRUE)
+pls.pro.m.grafen <- two.b.pls(pic.grafen.ind.m, pic.grafen.pro.m, verbose = TRUE)
+pls.pro.m.unit <- two.b.pls(pic.unit.ind.m, pic.unit.pro.m, verbose = TRUE)
+pls.jug.m.grafen <- two.b.pls(pic.grafen.ind.m, pic.grafen.jug.m, verbose = TRUE)
+pls.jug.m.unit <- two.b.pls(pic.unit.ind.m, pic.unit.jug.m, verbose = TRUE)
 
-## Female genitalia vs. female scutellum:
+## Female genitalia vs. female scutellum and pronotum:
 pls.scu.f.grafen <- two.b.pls(pic.grafen.ind.f, pic.grafen.scu.f, verbose = TRUE)
 pls.scu.f.unit <- two.b.pls(pic.unit.ind.f, pic.unit.scu.f, verbose = TRUE)
+pls.pro.f.grafen <- two.b.pls(pic.grafen.ind.f, pic.grafen.pro.f, verbose = TRUE)
+pls.pro.f.unit <- two.b.pls(pic.unit.ind.f, pic.unit.pro.f, verbose = TRUE)
+pls.jug.f.grafen$pvalue <- two.b.pls(pic.grafen.ind.f, pic.grafen.jug.f, verbose = TRUE)
+pls.jug.f.unit <- two.b.pls(pic.unit.ind.f, pic.unit.jug.f, verbose = TRUE)
 
 ## Making result plots:
 
 ## Plotting the first dimension of PLS results.
+pdf("PLS_plots.pdf", width = 3, height = 9)
 par(mfrow = c(3,1))
-cex <- 2.5
+cex <- 2
+
+## Male and female genitalia.
+par(mar=c(2.5,4,2.5,2))
 two.b.pls.plot(pls.ind.grafen, "PLS1 of male genitalia contrasts", "PLS1 of female genitalia contrasts", cex)
-two.b.pls.plot(pls.scu.m.grafen, "PLS1 of male scutellum contrasts", "PLS1 of male genitalia contrasts", cex)
-two.b.pls.plot(pls.scu.f.grafen, "PLS1 of female scutellum contrasts", "PLS1 of female genitalia contrasts", cex)
-dev.copy2pdf()
+
+## Male and somatic.
+up1 <- round(max(pls.scu.m.grafen$x.scores), digits = 1) + 0.1
+up2 <- round(max(pls.pro.m.grafen$x.scores), digits = 1) + 0.1
+up <- max(up1,up2)
+par(mar=c(2.5,4,2.5,2))
+plot(pls.scu.m.grafen$x.scores, pls.scu.m.grafen$y.scores, pch = 21, col = "grey"
+   , xlab = "", ylab = "", axes = FALSE, xlim = c(-up,up), ylim = c(-up,up), cex = cex)
+points(pls.pro.m.grafen$x.scores, pls.pro.m.grafen$y.scores, cex = cex, pch = 24, col = "grey")
+points(pls.jug.m.grafen$x.scores, pls.jug.m.grafen$y.scores, cex = cex, pch = 23, col = "grey")
+abline(lm(pls.scu.m.grafen$y.scores ~ pls.scu.m.grafen$x.scores), lwd = cex, col = "black")
+abline(lm(pls.pro.m.grafen$y.scores ~ pls.pro.m.grafen$x.scores), lwd = cex, lty = 1, col = "black")
+abline(lm(pls.jug.m.grafen$y.scores ~ pls.jug.m.grafen$x.scores), lwd = cex, lty = 1, col = "black")
+axis(side = 1); axis(side = 2)
+mtext(side = 1, text = "PLS1 of male somatic contrasts", line = 2.5)
+mtext(side = 2, text = "PLS1 of male genitalia contrasts", line = 2.5)
+
+## Female and somatic.
+up1 <- round(max(pls.scu.f.grafen$x.scores), digits = 1) + 0.1
+up2 <- round(max(pls.pro.f.grafen$x.scores), digits = 1) + 0.1
+up <- max(up1,up2)
+par(mar=c(2.5,4,2.5,2))
+plot(pls.scu.f.grafen$x.scores, pls.scu.f.grafen$y.scores, pch = 21, col = "grey"
+   , xlab = "", ylab = "", axes = FALSE, xlim = c(-up,up), ylim = c(-up,up), cex = cex)
+points(pls.pro.f.grafen$x.scores, pls.pro.f.grafen$y.scores, cex = cex, pch = 24, col = "grey")
+points(pls.jug.f.grafen$x.scores, pls.jug.f.grafen$y.scores, cex = cex, pch = 23, col = "grey")
+abline(lm(pls.scu.f.grafen$y.scores ~ pls.scu.f.grafen$x.scores), lwd = cex, col = "black")
+abline(lm(pls.pro.f.grafen$y.scores ~ pls.pro.f.grafen$x.scores), lwd = cex, lty = 1, col = "black")
+abline(lm(pls.jug.f.grafen$y.scores ~ pls.jug.f.grafen$x.scores), lwd = cex, lty = 1, col = "black")
+axis(side = 1); axis(side = 2)
+mtext(side = 1, text = "PLS1 of female somatic contrasts", line = 2.5)
+mtext(side = 2, text = "PLS1 of female genitalia contrasts", line = 2.5)
+
+dev.off()
